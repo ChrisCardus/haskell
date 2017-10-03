9bcf371cdd226bff8fc37c5b86d8a429727b80e3
Chris Cardus <cxc357@student.bham.ac.uk>
2017-10-03 11:56:36 +0100

    created formative1.hs and imported the Lib.hs which it requires

---
+++ formative/Formative1.hs
@@ -0,0 +1,28 @@
+-- This header must be kept exactly as it is for your submission to be valid:
+
+{-# LANGUAGE Safe #-} 
+
+
+module Formative1
+    (encode, decode, toTree, toTable)
+where
+
+import safe Lib
+
+-- End of header.
+
+
+encode :: String -> [MorseUnit]
+encode = undefined 
+
+
+decode :: [MorseUnit] -> String
+decode = undefined 
+
+
+toTree :: MorseTable -> MorseTree
+toTree = undefined
+
+
+toTable :: MorseTree -> MorseTable
+toTable = undefined
