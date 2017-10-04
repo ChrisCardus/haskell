b6f3e9abc293fb40d373d42747291a297284d079
Chris Cardus <cxc357@bham.ac.uk>
2017-10-04 13:23:50 +0100

    Completed encode and started on decode

--- formative/Formative1.hs
+++ formative/Formative1.hs
@@ -11,13 +11,27 @@ import safe Lib
 
 -- End of header.
 
+isLower :: Char -> Bool
+isLower c = elem c ['a'..'z']
+
+
+toUpper :: Char -> Char
+toUpper c = if isLower c then toEnum (fromEnum c - 32) else c
+
+
+{-isDit :: [MorseUnit] -> Bool
+isDit [] = False
+isDit (x:y:xs) = if x == Beep && y == Silence then True else False-}
+
 
 encode :: String -> [MorseUnit]
-encode = undefined 
+encode [] = []
+encode (x:xs) = if x /= ' ' then codeSymbol (toUpper x) ++ shortGap ++ encode (xs) else mediumGap ++ encode (xs)
 
 
 decode :: [MorseUnit] -> String
-decode = undefined 
+decode [] = []
+decode (x:xs) = 
 
 
 toTree :: MorseTable -> MorseTree
