bc192191505eed193591aad7d82d7e30c0c26eec
Chris Cardus <cxc357@bham.ac.uk>
2017-10-08 11:12:03 +0100

    Started work on toTree and added some helper functions

--- formative/Formative1.hs
+++ formative/Formative1.hs
@@ -19,6 +19,11 @@ toUpper :: Char -> Char
 toUpper c = if isLower c then toEnum (fromEnum c - 32) else c
 
 
+encode :: String -> [MorseUnit]
+encode [] = []
+encode (x:xs) = if x /= ' ' then codeSymbol (toUpper x) ++ shortGap ++ encode (xs) else mediumGap ++ encode (xs)
+
+
 fromJust :: Maybe Char -> String
 fromJust (Just a) = [a]
 fromJust Nothing = ""
@@ -30,15 +35,11 @@ findLetter xs | xs == [] = []
               | take 2 xs == [Beep, Silence] = dit ++ findLetter (drop 2 xs)
               | xs /= [] = []
 
+
 findGap :: [MorseUnit] -> [MorseUnit]
 findGap xs | take 4 xs == [Silence, Silence, Silence, Silence] = mediumGap
-          | take 2 xs == [Silence, Silence] = shortGap
-          | xs /= [] || xs == [] = []
-
-
-encode :: String -> [MorseUnit]
-encode [] = []
-encode (x:xs) = if x /= ' ' then codeSymbol (toUpper x) ++ shortGap ++ encode (xs) else mediumGap ++ encode (xs)
+           | take 2 xs == [Silence, Silence] = shortGap
+           | xs /= [] || xs == [] = []
 
 
 decode :: [MorseUnit] -> String
@@ -49,17 +50,31 @@ decode xs | lookup (findLetter xs) morseTable /= Nothing = fromJust (lookup (fin
           | xs /= [] = ""
 
 
-address :: [MorseUnit] -> String
-address [] = []
-address x = if take 2 x == dit then "l" ++ address (drop 2 x) else "r" ++ address (drop 4 x)
+data Direction = L | R deriving (Show, Eq, Ord)
+type Address   = [Direction]
 
 
-sortMorseList :: [MorseList] -> [MorseList]
-sortMorseList [] = []
-sortMorseList (x:xs) = 
+address :: [MorseUnit] -> Address
+address xs | xs == [] = []
+           | take 4 xs == [Beep, Beep, Beep, Silence] = [R] ++ address (drop 4 xs)
+           | take 2 xs == [Beep, Silence] = [L] ++ address (drop 2 xs)
+
+
+addressList :: MorseTable -> [Address]
+addressList [] = []
+addressList (x:xs) = (address (fst x)) : addressList xs
+
+
+sortAddressList :: [Address] -> [Address]
+sortAddressList [] = []
+sortAddressList (x:xs) = sortAddressList smaller ++ [x] ++ sortAddressList larger
+                         where
+                            smaller = [a | a <- xs, a <= x]
+                            larger = [b | b <- xs, b > x]
 
 
 toTree :: MorseTable -> MorseTree
+toTree [] = Nil
 toTree [x] = Leaf (snd x)
 toTree (x:xs) = Branch1 (snd x) (toTree xs) (toTree xs)
 
