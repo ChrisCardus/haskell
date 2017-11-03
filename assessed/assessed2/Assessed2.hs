e58627898c00aef2a4e187cd1b714351803ec510
Chris Cardus <cxc357@student.bham.ac.uk>
2017-11-03 13:04:50 +0000

    completed Assessed part 1&2

--- assessed/assessed2/Assessed2.hs
+++ assessed/assessed2/Assessed2.hs
@@ -5,18 +5,27 @@ module Assessed2
 where
 
 import safe Lib
+import safe Forest -- Remove before submit
+import safe K -- Remove before submit
 
 -- Q1
 isInTree :: Ord a => a -> Tree a -> Bool
-isInTree = undefined
+isInTree _ Empty = False
+isInTree x (Branch [] t') = isInTree x t'
+isInTree x (Branch (t:ts) t') = if x == snd t then True else if x > snd t then isInTree x (Branch ts t') else isInTree x (fst t)
 
 -- Q2
 tlookup :: Ord k => k -> Tree (Record k v) -> Maybe v
-tlookup = undefined
+tlookup x Empty = Nothing
+tlookup x (Branch [] t') = tlookup x t'
+tlookup x (Branch ((t,(Pair k v)):ts) t') = if x == k then Just v else if x > k then tlookup x (Branch ts t') else tlookup x t
 
 -- Q3
 rtlookup :: Ord k => (k,k) -> Tree (Record k v) -> [v]
-rtlookup = undefined
+rtlookup x Empty = []
+rtlookup (l, m) (Branch ((t,(Pair k v)):ts) t') = if l <= k && k <= m
+                                                  then rtlookup (l, m) t
+                                                  else rtlookup (l,m) (Branch ts t')
 
 -- Q4
 insertion :: Ord a => (a,a) -> Tree a -> Tree a
